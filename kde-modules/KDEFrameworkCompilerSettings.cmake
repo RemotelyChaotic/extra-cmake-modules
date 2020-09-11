ca4a102ec0bc7b211040233eb41e3a7e393a9821
Remotely Chaotic <jtek.fdlt@gmail.com>
2020-09-11 11:14:05 +0200

    JOIPEngine do not define QT_USE_QSTRINGBUILDER

--- kde-modules/KDEFrameworkCompilerSettings.cmake
+++ kde-modules/KDEFrameworkCompilerSettings.cmake
@@ -32,7 +32,7 @@ add_definitions(#-DQT_NO_CAST_TO_ASCII
                 #-DQT_NO_URL_CAST_FROM_STRING
                 #-DQT_NO_CAST_FROM_BYTEARRAY
                 #-DQT_NO_SIGNALS_SLOTS_KEYWORDS
-                -DQT_USE_QSTRINGBUILDER
+                #-DQT_USE_QSTRINGBUILDER
                 -DQT_NO_NARROWING_CONVERSIONS_IN_CONNECT
                )
 
